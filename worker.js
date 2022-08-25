065bb9cc86b3c872a2039187c593a758401dea9e
Nathan Clevenger <nateclev@gmail.com>
2022-08-25 06:12:45 -0500

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,10 @@
+import { JSONPath } from 'jsonpath-plus'
+
+export default {
+  fetch: async req => {
+    const { pathname, search } = new URL(req.url)
+    const [_, path, ...target] = pathname.split('/')
+    const json = await fetch('https://' + target.join('/') + search, req).then(res => res.json())
+    return new Response(JSON.stringify(JSONPath({path, json, preventEval: true}), null, 2), { headers: { 'content-type': 'application/json' }})
+  }
+}
