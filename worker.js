2382a5f10dc7f545a3b5be194736e5b6de9016d2
Nathan Clevenger <nateclev@gmail.com>
2022-09-03 11:18:44 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -5,6 +5,6 @@ export default {
     const { pathname, search } = new URL(req.url)
     const [_, path, ...target] = pathname.split('/')
     const json = await fetch('https://' + target.join('/') + search, req).then(res => res.json())
-    return new Response(JSON.stringify(JSONPath({path, json, preventEval: true}), null, 2), { headers: { 'content-type': 'application/json; charset=utf8' }})
+    return new Response(JSON.stringify(JSONPath({path, json, preventEval: true, flatten: true}), null, 2), { headers: { 'content-type': 'application/json; charset=utf8' }})
   }
 }
